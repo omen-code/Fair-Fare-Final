944de35fbe55bf4d66e9d471c2334b487e0f75a8
omen-code <nishhegde123@gmail.com>
2024-11-20 11:58:18 +0530

    map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,11 @@ import Navbar from "./components/Navbar/Navbar"
 import Home from "./pages/Home/Home"
 import { Routes, Route } from "react-router-dom"
 import Profile from "./pages/profile/profile"
-import Tc from "./pages/tc/Tc"
+import Tc from "./pages/TermsAndConditions/TermsAndConditions"
+import Confirmation from "./pages/confirmation/confirmation"
+import Login from "./pages/login/login"
+import SavedLocations from "./pages/savedlocations/savedlocation"
+import Aboutus from "./pages/About/About"
 
 const App = () => {
   return(
@@ -13,10 +17,10 @@ const App = () => {
     <Route path='/' element={<Home/>} />
     <Route path='/profile' element={<Profile/>}/>
     <Route path='/login' element={<Login/>}/>
-    <Route path='/confirmation' element={<Conformation/>}/>
-    <Route path='/savedlocations' element={<SavedLocations/>}/>
-    <Route path='/about_us' element={<FFAbout/>}/>
-    <Route path='/confirmation' element={<Conformation/>}/>
+    <Route path='/confirmation' element={<Confirmation/>}/>
+    <Route path='/saved-locations' element={<SavedLocations/>}/>
+    <Route path='/about_us' element={<Aboutus/>}/>
+    <Route path='/confirmation' element={<Confirmation/>}/>
     <Route path='/tc' element={<Tc/>}/>
     </Routes>
     <Footer />
