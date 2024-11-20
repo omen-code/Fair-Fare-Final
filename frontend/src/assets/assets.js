64a574624b8889dfab2b3da895cce71efc82aaa8
rjune <mallikarjunreddy0564@gmail.com>
2024-11-20 10:45:09 +0530

    asssets

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,34 @@
+import map_icon from './map_icon.png'
+import insta_icon from './insta_icon.jpg'
+import facebook_logo from './facebook_logo.png'
+import x_logo from './x_logo.png'
+
+import car_pic from './car picture.png'
+import minicar from './minicar.jpg'
+import ola from './ola_icon.jpg'
+
+import rapido from './rapido_icon.jpg'
+import uber from './uber_icon.jpg'
+import auto from './taxiauto.jpg'
+import bike from './taxibike.webp'
+import qr from './qr.jpg'
+import user_icon from './user_icon.png'
+
+
+
+export const assets = {
+    map_icon,
+    insta_icon,
+    facebook_logo,
+    x_logo,
+    car_pic,
+    minicar,
+    ola,
+    rapido,
+    uber,
+    auto,
+    bike,
+    qr,
+    user_icon
+}
+
