ac939fc54c195c5fc853682b24645b84518784cf
rjune <mallikarjunreddy0564@gmail.com>
2024-11-20 11:39:06 +0530

    hi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,27 @@
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import Home from "./pages/Home/Home";
-import Profile from "./pages/Profile/Profile"; // Import Profile page
-import Wallet from "./pages/Wallet/Wallet"; // Import Wallet page
-import Login from "./pages/Login/Login"; // Import Login page
-import SavedLocations from "./pages/SavedLocations/SavedLocation"; // Import Saved Locations page
-import TermsAndConditions from "./pages/TermsAndConditions/TermsAndConditions"; // Import Terms & Conditions page
-import { Routes, Route } from "react-router-dom";
-import Aboutus from "./pages/About/About";
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import { Routes, Route } from "react-router-dom"
+import Profile from "./pages/profile/profile"
+import Tc from "./pages/tc/Tc"
 
 const App = () => {
-  return (
+  return(
     <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} /> {/* Home Page */}
-        <Route path="/profile" element={<Profile />} /> {/* Profile Page */}
-        <Route path="/login" element={<Login />} /> {/* Login Page */}
-        <Route path="/saved-locations" element={<SavedLocations />} />{" "}
-        {/* Saved Locations Page */}
-        <Route path="/wallet" element={<Wallet />} /> {/* Wallet Page */}
-        <Route path="/about" element={<Aboutus />} /> {/* About Us Page */}
-        <Route
-          path="/terms-and-conditions"
-          element={<TermsAndConditions />}
-        />{" "}
-        {/* Terms & Conditions Page */}
-      </Routes>
-      <Footer />
+  <Navbar/>
+    <Routes>
+    <Route path='/' element={<Home/>} />
+    <Route path='/profile' element={<Profile/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/confirmation' element={<Conformation/>}/>
+    <Route path='/savedlocations' element={<SavedLocations/>}/>
+    <Route path='/about_us' element={<FFAbout/>}/>
+    <Route path='/confirmation' element={<Conformation/>}/>
+    <Route path='/tc' element={<Tc/>}/>
+    </Routes>
+    <Footer />
     </>
-  );
-};
+  )
+} 
 
-export default App;
+export default App
